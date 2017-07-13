449fcb61dc6bf98728feea64c73eff2c78337f07
reborn <reborn.geralt@gmail.com>
2017-07-13 20:57:35 +0200

    first comit

---
+++ scripts/try.js
@@ -0,0 +1,15 @@
+var natural = require('natural');
+
+(function() {
+  module.exports = function(robot) {
+    return robot.listen(function(message) {
+      if (!message.text) {
+        return;
+      }
+      return message.user.name === "Steve" && Math.random() > 0.8;
+    }, function(response) {
+      return response.reply("HI STEVE! YOU'RE MY BEST FRIEND! (but only like " + (response.match * 100) + "% of the time)");
+    });
+  };
+
+}).call(this);
